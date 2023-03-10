b886eb3c742ad264f76087e2edde27d4c84e2e72
Kushagraved <kushagraved@gmail.com>
2023-03-10 22:45:32 +0530

    fix: minor upgrades

--- tests/collectionService.test.js
+++ tests/collectionService.test.js
@@ -1,5 +1,5 @@
-const db = require('../database/models')
-const { createNewEntryService } = require('../src/services/collectionService');
+const db = require('../database/models');
+const { createNewEntryService, updateEntryService } = require('../src/services/collectionService');
 describe('collection service', () => {
   it('should create a new entry ', async () => {
     jest.spyOn(db.Collection, 'create').mockResolvedValue({
@@ -20,6 +20,40 @@ describe('collection service', () => {
         description: 'test description'
       }
     });
+
+
   });
+  it('should update a new entry', async () => {
+    jest.spyOn(db.Collection, 'findOne').mockResolvedValue({
+      id: 1,
+      contentTypeId: 1,
+      entries: {
+        title: 'test title',
+        description: 'test description'
+      }
+    }
+    );
+    jest.spyOn(db.Collection, 'update').mockResolvedValue([1, {
+      id: 1,
+      contentTypeId: 1,
+      entries: {
+        title: 'test title',
+        description: 'test description'
+      }
+    }]
+    );
+
+    const updatedEntry = await updateEntryService(1, 1, {});
+    expect(updatedEntry).toEqual({
+      id: 1,
+      contentTypeId: 1,
+      entries: {
+        title: 'test title',
+        description: 'test description'
+      }
+    });
+  });
+
 });
+
 
